eaa914ad158791f94a01ee7a266ed8215d5031ba
민준규 <wnsrb1509@gmail.com>
2025-05-26 19:53:58 +0900

    25-05-26

--- Model/gru.py
+++ Model/gru.py
@@ -85,15 +85,39 @@ def add_rolling_features(df):
         print(f"[series_id={sid}] 가격 범위: {group['avg_price'].min():,.0f} ~ {group['avg_price'].max():,.0f}")
         # print(f"[name={name}] 가격 범위: {group['avg_price'].min():,.0f} ~ {group['avg_price'].max():,.0f}")
 
-        scaler_price = MinMaxScaler()
-        scaler_mean = MinMaxScaler()
-        scaler_std = MinMaxScaler()
+        # # A. 데이터 전체
+        # scaler_price = MinMaxScaler()
+        # scaler_mean = MinMaxScaler()
+        # scaler_std = MinMaxScaler()
+        # df.loc[group.index, 'price_scaled'] = scaler_price.fit_transform(group[['avg_price']])
+        # df.loc[group.index, 'mean_scaled_7'] = scaler_mean.fit_transform(group[['rolling_mean_7']])
+        # df.loc[group.index, 'std_scaled_7'] = scaler_std.fit_transform(group[['rolling_std_7']])
+        # df.loc[group.index, 'mean_scaled_30'] = scaler_mean.fit_transform(group[['rolling_mean_30']])
+        # df.loc[group.index, 'std_scaled_30'] = scaler_std.fit_transform(group[['rolling_std_30']])
 
-        df.loc[group.index, 'price_scaled'] = scaler_price.fit_transform(group[['avg_price']])
-        df.loc[group.index, 'mean_scaled_7'] = scaler_mean.fit_transform(group[['rolling_mean_7']])
-        df.loc[group.index, 'std_scaled_7'] = scaler_std.fit_transform(group[['rolling_std_7']])
-        df.loc[group.index, 'mean_scaled_30'] = scaler_mean.fit_transform(group[['rolling_mean_30']])
-        df.loc[group.index, 'std_scaled_30'] = scaler_std.fit_transform(group[['rolling_std_30']])
+        # B. 이상치 제외한 범위로 정규화 스케일러 fit
+        q_low = group['avg_price'].quantile(0.01)
+        q_high = group['avg_price'].quantile(0.99)
+        trimmed = group[(group['avg_price'] >= q_low) & (group['avg_price'] <= q_high)]
+
+        # ✅ 각각 fit
+        scaler_price = MinMaxScaler()
+        scaler_price.fit(trimmed[['avg_price']])
+        scaler_mean = MinMaxScaler()
+        scaler_mean.fit(trimmed[['rolling_mean_7']])
+        scaler_std = MinMaxScaler()
+        scaler_std.fit(trimmed[['rolling_std_7']])
+        scaler_mean_30 = MinMaxScaler()
+        scaler_mean_30.fit(trimmed[['rolling_mean_30']])
+        scaler_std_30 = MinMaxScaler()
+        scaler_std_30.fit(trimmed[['rolling_std_30']])
+
+        # ✅ 전체 데이터 transform
+        df.loc[group.index, 'price_scaled'] = scaler_price.transform(group[['avg_price']])
+        df.loc[group.index, 'mean_scaled_7'] = scaler_mean.transform(group[['rolling_mean_7']])
+        df.loc[group.index, 'std_scaled_7'] = scaler_std.transform(group[['rolling_std_7']])
+        df.loc[group.index, 'mean_scaled_30'] = scaler_mean_30.transform(group[['rolling_mean_30']])
+        df.loc[group.index, 'std_scaled_30'] = scaler_std_30.transform(group[['rolling_std_30']])
 
         scaler[sid] = { # A. series scale
         # scaler[name] = { # B. name scale
